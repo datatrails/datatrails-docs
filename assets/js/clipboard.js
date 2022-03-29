c30614334b2562d0ae45e030aec4a2b3229124f4
William Godfrey <sernineleflambeur@gmail.com>
2022-03-29 16:58:28 +0100

    Adding Clipboard Functinality

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -1,16 +1,33 @@
 import Clipboard from 'clipboard';
 
-var clipboard = new Clipboard('.btn-clipboard', {
-    target: function (trigger) {
-      return trigger.nextElementSibling;
-    },
-  });
+var pre = document.getElementsByTagName('pre');
+
+for (var i = 0; i < pre.length; ++ i)
+{
+  var element = pre[i];
+  var mermaid = element.getElementsByClassName('language-mermaid')[0];
+
+  if (mermaid == null) {
+    element.insertAdjacentHTML('afterbegin', '<button class="btn btn-copy"></button>');
+  }
+}
+
+var clipboard = new Clipboard('.btn-copy', {
+
+  target: function(trigger) {
+    return trigger.nextElementSibling;
+  },
+
+});
 
 clipboard.on('success', function(e) {
-    
+
+    /*
     console.info('Action:', e.action);
     console.info('Text:', e.text);
     console.info('Trigger:', e.trigger);
+    */
+
     e.clearSelection();
 });
 
@@ -18,15 +35,3 @@ clipboard.on('error', function(e) {
     console.error('Action:', e.action);
     console.error('Trigger:', e.trigger);
 });
-
-
-var pre = document.getElementsByTagName('pre');
-for (var i = 0; i < pre.length; i++) {
-    var isLanguage = pre[i].children[0].className.indexOf('language-');
-    if ( isLanguage === 0 ) {
-        var button     = document.createElement("button")
-            button.className='btn-clipboard btn btn-link d-none d-lg-block'
-            button.innerHTML='<span class="copy-status"></span>'
-            pre[i].prepend(button)
-        }
-}
